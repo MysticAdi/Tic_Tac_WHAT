3eabb24d1e1559fd89dc547ed8985fde76728d34
MysticAdi <adityapt97@gmail.com>
2020-01-02 19:35:22 +0530

    Added Formats

--- original_game.cpp
+++ original_game.cpp
@@ -1,21 +1,42 @@
 //The original version to check for overflow complications of size greater than 3 and player switches.
 
 #include<iostream>
+#include<string>
 using namespace std;
 #define size 3
-
-int initialise_grid()
+void put_line()
+{
+	for(int i=0;i<7;i++)
+	{
+		cout<<"-";
+	}
+	cout<<endl;
+}
+void put_first()
+{
+	cout<<"|";
+}
+void initialise_grid()
 {
 	char grid[size][size];
-	int num=1;
+	int num=49;
 	for(int i=0;i<size;i++)
-	{
+	{	
+
+		put_line();
+		put_first();
 		for(int j=0;j<size;j++)
-		{
-			char[i][j]=
+		{	
+			char number = (char)num;
+			grid[i][j]=number;
+			cout<<grid[i][j]<<"|";
+			num++;
 		}
+		cout<<endl;
 	}
+	put_line();
 }
+
 int main()
 {
 	initialise_grid();
